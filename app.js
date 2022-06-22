a0453187e94b52a5de99b1fd7c835d0980d85538
SantiMit1 <37678817+SantiMit1@users.noreply.github.com>
2022-06-22 20:33:14 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,5 +5,4 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.use(express.static(path.resolve(__dirname, "./public")))
-console.log(path.resolve(__dirname, "./public"))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
