4fae8ac92dc617070854e730d25fbafd84705ffa
SantiMit1 <santimiti10@gmail.com>
2022-06-23 21:28:00 -0300

    estructura basico de home.html

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "./views/home.html")));
 app.use(express.static(path.resolve(__dirname, "./public")))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
