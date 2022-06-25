4fb6ebcd02b352fe56ee5b7f4502c06a9bf986b5
SantiMit1 <santimiti10@gmail.com>
2022-06-25 04:37:35 -0300

    login y signup terminados

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "./views/home.html")));
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, "./views/login.html")));
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, "./views/register.html")));
 app.use(express.static(path.resolve(__dirname, "./public")))
 app.listen(port, () => console.log(`app listening on port ${port}!`));
